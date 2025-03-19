b567e8dc469d13455b50c79613891127d0271cbe
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-19 18:21:05 +0100

    Add aria label to cells & alt attr to images

--- src/ui/renderMine.ts
+++ src/ui/renderMine.ts
@@ -4,6 +4,7 @@ export const renderMine = (cell: Cell, cellSquare: HTMLButtonElement) => {
   const mine = document.createElement("img") as HTMLImageElement;
   mine.classList.add("hidden");
   mine.src = "../images/dabomb.svg";
+  mine.alt = "";
 
   cellSquare.appendChild(mine);
 };
