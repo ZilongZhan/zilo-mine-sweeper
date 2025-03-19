a050d575f7aaae9df4233e137b3d038310d1343a
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-19 21:22:25 +0100

    Degrain generateBoard test into smaller tests

--- src/game/__tests__/generateBoard.test.ts
+++ src/game/__tests__/generateBoard.test.ts
@@ -2,49 +2,45 @@ import { generateBoard } from "../generateBoard";
 
 describe("Given the generateBoard function", () => {
   describe("When it receives 5", () => {
-    test("Then it should return an array of Cell arrays of length 5 each, with no mines and no adjacent mines", () => {
-      const dimensions = 5;
-      const expectedBoard = [
-        [
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-        ],
-        [
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-        ],
-        [
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-        ],
-        [
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-        ],
-        [
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-          { hasMine: false, adjacentMinesTotal: 0 },
-        ],
-      ];
+    const dimensions = 5;
 
+    test("Then it should return an board of rows length 5.", () => {
+      const expectedLength = 5;
+
+      const arrayLength = generateBoard(dimensions).length;
+
+      expect(arrayLength).toBe(expectedLength);
+    });
+
+    test("Then it should return an board of columns with length 5.", () => {
+      const expectedRowLength = 5;
       const board = generateBoard(dimensions);
 
-      expect(board).toStrictEqual(expectedBoard);
+      const areAllColumnsCorrectLength = board.every(
+        (row) => row.length === expectedRowLength
+      );
+
+      expect(areAllColumnsCorrectLength).toBe(true);
+    });
+
+    test("Then it should return a board of cells with no mines.", () => {
+      const board = generateBoard(dimensions);
+
+      const haveNoMines = board.every((row) =>
+        row.every((cell) => cell.hasMine === false)
+      );
+
+      expect(haveNoMines).toBe(true);
+    });
+
+    test("Then it should return a board of cells with no adjacent mines", () => {
+      const board = generateBoard(dimensions);
+
+      const haveNoAdjacentMines = board.every((row) =>
+        row.every((cell) => cell.adjacentMinesTotal === 0)
+      );
+
+      expect(haveNoAdjacentMines).toBe(true);
     });
   });
 
