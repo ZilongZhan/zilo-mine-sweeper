b7378bd04d200ef668e5c4acf04a2e30a9ad10ff
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-19 18:37:42 +0100

    Add guard clause validation in renderBoard function

--- src/ui/renderBoard.ts
+++ src/ui/renderBoard.ts
@@ -2,14 +2,19 @@ import { Board } from "../data/types.js";
 import { renderCell } from "./renderCell.js";
 
 export const renderBoard = (board: Board): void => {
-  const gameBoard = document.querySelector(".board") as HTMLElement;
-  const boardRow = document.querySelector(".row") as HTMLUListElement;
-  const dummyCell = document.querySelector(".row li") as HTMLLIElement;
+  const gameBoard = document.querySelector(".board");
+  const boardRow = document.querySelector(".row");
+  const dummyCell = document.querySelector(".row li");
+
+  if (!gameBoard || !boardRow || !dummyCell) {
+    throw new Error("Missing required elements.");
+  }
+
   gameBoard.innerHTML = "";
   boardRow.innerHTML = "";
 
   board.forEach((row) => {
-    const newRow = boardRow.cloneNode(true) as HTMLLIElement;
+    const newRow = boardRow.cloneNode(true);
 
     row.forEach((cell) => {
       const newCell = dummyCell.cloneNode(true) as HTMLLIElement;
