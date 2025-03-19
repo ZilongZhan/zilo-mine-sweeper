dc080d5b44d30604d0ed34087d7ac899db9942cb
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-19 21:29:54 +0100

    Change variable names for better consistency

--- src/game/__tests__/generateBoard.test.ts
+++ src/game/__tests__/generateBoard.test.ts
@@ -3,29 +3,27 @@ import { generateBoard } from "../generateBoard";
 describe("Given the generateBoard function", () => {
   describe("When it receives 5", () => {
     const dimensions = 5;
+    const board = generateBoard(dimensions);
 
     test("Then it should return an board of rows with length 5.", () => {
-      const expectedLength = 5;
+      const expectedRowLength = 5;
 
-      const arrayLength = generateBoard(dimensions).length;
+      const rowLength = board.length;
 
-      expect(arrayLength).toBe(expectedLength);
+      expect(rowLength).toBe(expectedRowLength);
     });
 
     test("Then it should return an board of columns with length 5.", () => {
-      const expectedRowLength = 5;
-      const board = generateBoard(dimensions);
+      const expectedColumnLength = 5;
 
       const areAllColumnsCorrectLength = board.every(
-        (row) => row.length === expectedRowLength
+        (row) => row.length === expectedColumnLength
       );
 
       expect(areAllColumnsCorrectLength).toBe(true);
     });
 
     test("Then it should return a board of cells with no mines.", () => {
-      const board = generateBoard(dimensions);
-
       const haveNoMines = board.every((row) =>
         row.every((cell) => cell.hasMine === false)
       );
@@ -34,8 +32,6 @@ describe("Given the generateBoard function", () => {
     });
 
     test("Then it should return a board of cells with no adjacent mines", () => {
-      const board = generateBoard(dimensions);
-
       const haveNoAdjacentMines = board.every((row) =>
         row.every((cell) => cell.adjacentMinesTotal === 0)
       );
