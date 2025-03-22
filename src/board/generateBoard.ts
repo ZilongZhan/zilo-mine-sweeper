7e01e3730258de2bf0cffc072f20c4e54cdc2d88
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-22 20:38:58 +0100

    Create board entity module

---
+++ src/board/generateBoard.ts
@@ -0,0 +1,28 @@
+import { createCell } from "../cell/createCell.js";
+import { Board } from "../data/types.js";
+
+export const generateBoard = (dimensions: number): Board => {
+  const areInvalidDimensions = dimensions < 5;
+
+  if (areInvalidDimensions) {
+    throw new Error("Dimensions cannot be smaller than 5");
+  }
+
+  if (!Number.isInteger(dimensions)) {
+    throw new Error("Function can only accept integers");
+  }
+
+  const board: Board = [];
+
+  for (let rowNumber = 0; rowNumber < dimensions; rowNumber++) {
+    board.push([]);
+
+    for (let columnNumber = 0; columnNumber < dimensions; columnNumber++) {
+      const cell = createCell(rowNumber, columnNumber);
+
+      board[rowNumber][columnNumber] = cell;
+    }
+  }
+
+  return board;
+};
