76c761e97d31af84f6bb0819a7ed4dc5404cbbd3
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-22 22:59:18 +0100

    Add gameOver funtion

--- src/ui/gameOver.ts
+++ src/ui/gameOver.ts
@@ -1,7 +1,11 @@
+import { renderModalElement } from "./renderModalElement.js";
+
 export const gameOver = (): void => {
   const mineElements = document.querySelectorAll(".mine");
 
   mineElements.forEach((mineElement) => {
     mineElement.classList.remove("hidden");
   });
+
+  renderModalElement("lose");
 };
