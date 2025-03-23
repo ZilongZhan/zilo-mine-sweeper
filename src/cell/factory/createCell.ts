98ddb79e81dc2b60940453281cd746ae230c7534
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-24 00:30:34 +0100

    Define tests for createCell function

--- src/cell/factory/createCell.ts
+++ src/cell/factory/createCell.ts
@@ -1,6 +1,14 @@
 import { Cell } from "../../data/types";
 
 export const createCell = (rowNumber: number, columnNumber: number): Cell => {
+  if (!Number.isInteger(rowNumber) || !Number.isInteger(columnNumber)) {
+    throw new Error("Function only accepts integers.");
+  }
+
+  if (rowNumber < 0 || columnNumber < 0) {
+    throw new Error("Cell cannot have negative position.");
+  }
+
   const cell: Cell = {
     hasMine: false,
     adjacentMinesTotal: 0,
