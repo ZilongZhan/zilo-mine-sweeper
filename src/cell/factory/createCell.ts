163fa99802f387c7395fbf496558f3fd9f185cce
Zilong Zhan <zolong.zhan@gmail.com>
2025-03-23 20:46:44 +0100

    Add factory folder to cell entity module

--- src/cell/factory/createCell.ts
+++ src/cell/factory/createCell.ts
@@ -1,4 +1,4 @@
-import { Cell } from "../data/types";
+import { Cell } from "../../data/types";
 
 export const createCell = (rowNumber: number, columnNumber: number): Cell => {
   const cell: Cell = {
